cecba82b4d7e12ecfd4cfb58f3ea6fe9160b63f0
saulo <saulolazarogarcia@gmail.com>
2025-08-26 12:27:10 -0300

    config terraform to gcp

--- terraform/environment/production/providers.tf
+++ terraform/environment/production/providers.tf
@@ -4,7 +4,7 @@ terraform {
 
   backend "gcs" {
     bucket = "fluid-terraform"
-    prefix = "fluid-droplet-NAME/production"
+    prefix = "fluid-droplet-shipstation/production"
   }
 
   required_providers {
