9477f3b154b9d73794382642a79055b4146b28f9
Gaston Massimino <gamassimino01@gmail.com>
2025-08-29 16:46:04 -0300

    check if column exist to avoid wrong deploys

--- db/migrate/20250717195312_add_droplet_installation_uuid_to_companies.rb
+++ db/migrate/20250717195312_add_droplet_installation_uuid_to_companies.rb
@@ -1,5 +1,7 @@
 class AddDropletInstallationUuidToCompanies < ActiveRecord::Migration[8.0]
   def change
-    add_column :companies, :droplet_installation_uuid, :string
+    unless column_exists?(:companies, :droplet_installation_uuid)
+      add_column :companies, :droplet_installation_uuid, :string
+    end
   end
 end
