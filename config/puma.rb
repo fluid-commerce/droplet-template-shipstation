2578024775b8d23ba4115a8bfd758868b0220cc7
Jake <jake.bliss96@gmail.com>
2025-10-24 20:54:21 -0400

    Enable Solid Queue supervisor in Puma for background job processing
    - Add Solid Queue supervisor startup in puma.rb when SOLID_QUEUE_IN_PUMA env var is set
    - This fixes the issue where OrderShippedJob and other background jobs were being enqueued but never processed
    - Jobs will now be processed by the Solid Queue worker running inside the first Puma worker

--- config/puma.rb
+++ config/puma.rb
@@ -16,4 +16,21 @@ environment ENV["RACK_ENV"] || "development"
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
+
+  # Start the Solid Queue supervisor in the first Puma worker
+  # if SOLID_QUEUE_IN_PUMA is set to true
+  if ENV["SOLID_QUEUE_IN_PUMA"]
+    Thread.new do
+      Rails.application.executor.wrap do
+        # Only start the supervisor in the first worker
+        if Puma.respond_to?(:cli_config) && Puma.cli_config.options[:worker_index] == 0
+          Rails.logger.info("Starting Solid Queue supervisor in Puma worker 0")
+          SolidQueue::Supervisor.start
+        end
+      end
+    rescue => e
+      Rails.logger.error("Failed to start Solid Queue supervisor: #{e.message}")
+      Rails.logger.error(e.backtrace.join("\n"))
+    end
+  end
 end
