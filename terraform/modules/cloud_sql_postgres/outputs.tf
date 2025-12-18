89bafc20056e66b5f162941fbe2797f78e5bb021
saulo <saulolazarogarcia@gmail.com>
2025-12-18 17:20:38 -0300

    remove old config modules tf

--- terraform/modules/cloud_sql_postgres/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "db_instance_name" {
-  description = "Name of the Cloud SQL instance"
-  value       = google_sql_database_instance.postgres.name
-}
--- terraform/modules/compute_address/outputs.tf
+++
@@ -1,5 +0,0 @@
-# address IP
-output "address" {
-  description = "La address IP"
-  value       = google_compute_address.static_ip.address
-}
